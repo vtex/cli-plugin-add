543a21ec1724da786805abd8cf82eea305d641f9
Thiago Veras <thiago.veras.machado@gmail.com>
2021-03-11 16:59:13 -0300

    Lint

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -2,7 +2,7 @@ import { CustomCommand } from 'vtex'
 import appsAdd from '../modules/add'
 
 export default class Add extends CustomCommand {
-  static description = 'Adds the specified app(s) to the manifest\'s dependencies.'
+  static description = "Adds the specified app(s) to the manifest's dependencies."
 
   static examples = ['vtex add vtex.service-example@0.x']
 
@@ -13,8 +13,19 @@ export default class Add extends CustomCommand {
   static strict = false
 
   static args = [
-    { name: 'appId', required: true, description: 'Name and version (`{vendor}.{appname}@{x.x.x}`) of the dependency to include in the manifest.json file.' },
-    { name: 'ithAppId', required: false, multiple: true, description: 'Names and versions (`{vendor}.{appname}@{x.x.x}`) of the multiple dependencies to include in the manifest.json file.' },
+    {
+      name: 'appId',
+      required: true,
+      description:
+        'Name and version (`{vendor}.{appname}@{x.x.x}`) of the dependency to include in the manifest.json file.',
+    },
+    {
+      name: 'ithAppId',
+      required: false,
+      multiple: true,
+      description:
+        'Names and versions (`{vendor}.{appname}@{x.x.x}`) of the multiple dependencies to include in the manifest.json file.',
+    },
   ]
 
   async run() {
